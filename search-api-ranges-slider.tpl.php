40a06b855e732787017f817cd7bf5cfcf433073a
Mathijs Koenraadt <info@morningtime.com>
2011-04-10 10:19:39 +0200

    Issue #1106664 by sammyd56:  = array().

--- search-api-ranges-slider.tpl.php
+++ search-api-ranges-slider.tpl.php
@@ -13,9 +13,9 @@
 ?>
 <?php print drupal_render($form['text-range']); ?>
 <div class="yui3-g">
-  <div class="yui3-u range-box"><?php print drupal_render($form['range-from']); ?></div>
+  <div class="yui3-u range-box range-box-left"><?php print drupal_render($form['range-from']); ?></div>
   <div class="yui3-u range-slider-box"><?php print drupal_render($form['range-slider']); ?></div>
-  <div class="yui3-u range-box"><?php print drupal_render($form['range-to']); ?></div>
+  <div class="yui3-u range-box range-box-right"><?php print drupal_render($form['range-to']); ?></div>
 </div>
 <?php print drupal_render($form['submit']); ?>
 <?php print drupal_render_children($form); ?>
