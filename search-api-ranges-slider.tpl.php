9bcae03f55f996dc43cb2310bd09ab2ef902c06d
tinker <tinker@200883.no-reply.drupal.org>
2012-11-18 11:07:27 +0100

    Issue #1825718 by tinker: coding standards cleanup.

--- search-api-ranges-slider.tpl.php
+++ search-api-ranges-slider.tpl.php
@@ -5,12 +5,18 @@
 ?>
 <?php print drupal_render($form['text-range']); ?>
 <div class="yui3-g">
-  <div class="yui3-u range-box range-box-left"><?php print drupal_render($form['range-from']); ?></div>
-  <div class="yui3-u range-slider-box"><?php print drupal_render($form['range-slider']); ?></div>
-  <div class="yui3-u range-box range-box-right"><?php print drupal_render($form['range-to']); ?></div>
+  <div class="yui3-u range-box range-box-left">
+    <?php print drupal_render($form['range-from']); ?>
+  </div>
+  <div class="yui3-u range-slider-box">
+    <?php print drupal_render($form['range-slider']); ?>
+  </div>
+  <div class="yui3-u range-box range-box-right">
+    <?php print drupal_render($form['range-to']); ?>
+  </div>
 </div>
 <?php print drupal_render($form['submit']); ?>
 <?php
-// render required hidden fields
-print drupal_render_children($form);
+  // Render required hidden fields.
+  print drupal_render_children($form);
 ?>
